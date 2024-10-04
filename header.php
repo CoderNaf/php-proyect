e59e4e14915ac946d1a179b279ba4afbad3d86ee
codernaf <produccionesbog@gmail.com>
2024-10-04 17:29:46 -0500

    first one proyect with php

---
+++ header.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+    if(isset($_SESSION['txtemail']) != 'codernaf'){
+        header('location:login.php');
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>portfolio php</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+
+    <div class="container">
+        <a href="index.php">home|</a>
+        <a href="portfolio.php">portafolio|</a>
+        <a href="close.php">cerrar seción</a>
+    
+    
---
+++ portfolio.php
@@ -0,0 +1,9 @@
+<?php include 'header.php' ?>
+
+    <h1>¡Hello World I'm portfolio!</h1>
+
+    
+
+<?php  include 'footer.php' ?>
+    
+
